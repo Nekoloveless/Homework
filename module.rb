9a8d1751cb15036cc9759e5cd3ece561c24848b0
Olga Romankiv <o.romankiv@gmail.com>
2015-02-18 23:33:51 +0200

    updated 18.02.2015 - added creation of two users during the test

--- module.rb
+++ module.rb
@@ -14,15 +14,24 @@ module RunScript
     @email = @name + '@rt.ty'
   end
 
+  def set_first_user
+    @first = @name
+  end
+
   def login
     @driver.find_element(class: 'register').click
     @driver.find_element(id: 'user_login').send_keys @name
     @driver.find_element(id: 'user_password').send_keys @password1
     @driver.find_element(id: 'user_password_confirmation').send_keys @password1
-    @driver.find_element(id: 'user_firstname').send_keys 'qwerqwer'
-    @driver.find_element(id: 'user_lastname').send_keys 'qwerqwer'
+    @driver.find_element(id: 'user_firstname').send_keys @name
+    @driver.find_element(id: 'user_lastname').send_keys @name
     @driver.find_element(id: 'user_mail').send_keys @email
     @driver.find_element(name: 'commit').click
+  end
+
+  def logout
+    @user_id = @driver.find_element(class: 'user active').attribute('value')
+    @driver.find_element(class: 'logout').click
   end
 
   def change_pass
@@ -34,31 +43,37 @@ module RunScript
     @driver.find_element(name: 'commit').click
   end
 
-  def last
-
+  def create_project
 @driver.find_element(class: 'projects').click
 @driver.find_element(:xpath,".//*[@id='content']/div[1]/a[1]").click
 @driver.find_element(id: 'project_name').send_keys @projectname
 @driver.find_element(name: 'commit').click
+  end
+
+  def add_user
 @driver.find_element(id: 'tab-members').click
-@driver.find_element(id: 'principal_search').send_keys 'olgao'
+@driver.find_element(id: 'principal_search').send_keys @first
 sleep 2
-@driver.find_element(:xpath,".//div[@id='principals']/label[.='olga olga']/input").click #на этом моменте скрипт не работает! //div[@id='principals']/label[.=' asdf asdf']/input
-@driver.find_element(name: 'membership[role_ids][]').click
+@driver.find_element(:xpath,".//div[@id='principals']/label[contains(text()," + @first +")]/input").click #вариант 2 .//div[@id='principals']/label[.=' olga olga']/input
+@driver.find_element(:xpath,".//label[.=' Manager']/input").click
 @driver.find_element(id: 'member-add-submit').click
-@driver.find_element(:xpath,".//*[@id='member-89953']/td[3]/a[1]").click
+  end
+
+  def change_role
+@driver.find_element(:xpath,".//href[contains(text(),'Редактировать']/input").click # теперь это место не работает
 @driver.find_element(:xpath,".//*[@id='content']/div[1]/a[1]").click
 @driver.find_element(:xpath,".//*[@id='member-89953-roles-form']/p[1]/label[2]/input").click
 @driver.find_element(name: 'commit').click
+  end
 
+  def last
 @driver.find_element(id: 'tab-versions').click
 @driver.find_element(:xpath,".//*[@id='tab-content-versions']/p[2]/a").click
 @driver.find_element(id: 'version_name').send_keys 'task 1'
 @driver.find_element(id: 'version_sharing').click
 @driver.find_element(:xpath,".//*[@id='version_sharing']/option[2]").click
 @driver.find_element(name: 'commit').click
-
-end
+  end
 
 
 
--- run_script.rb
+++ run_script.rb
@@ -5,7 +5,15 @@ include RunScript
 
 open_browser(:firefox)
 
-set_param('rfgtyhuxds', 'erthgyhry', 'erfgegh')
+set_param('thrhnvbkuio', 'erthgyhry', 'erfgegh')
+login
+logout
+set_first_user
+
+set_param('gbhtreddf', 'tghyrewe', 'efgwefgr')
 login
 change_pass
+create_project
+add_user
+change_role
 last
